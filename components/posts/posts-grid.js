c653125a535baac4536f241072d77ef125b2d64d
bruceliu <bruceliu1107@gmail.com>
2023-04-22 19:09:18 -0400

    post grid styling

---
+++ components/posts/posts-grid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from '/posts-grid.module.css'
+
+function PostsGrid() {
+  const {posts} = props;
+
+    return (
+    <ul className='classes.grid'>
+        {posts.map(post => <PostItem />)}
+    </ul>
+  )
+}
+
+export default PostsGrid
