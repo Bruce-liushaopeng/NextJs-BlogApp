e8d1a90e855dc78f7750ed668957cea05c9cd943
bruceliu <bruceliu1107@gmail.com>
2023-05-02 15:12:02 -0400

    testing fail

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 // this is equivalent of doing export default in the front end
 // env can be accessed both front end and backend by process.env.something
-const { PHASE_DEVELOPMENT_SERVER,} = require("next/constants");
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 // we make it a function, so that we can use if statement to check constants(PHASE)
 module.exports = (phase) => {
-    if(phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-            env: {
-                mongodb_username: "primaryclient",
-                mongodb_password: "ppclient",
-                mongodb_cluster: "cluster0",
-                mongodb_database: "my-site-dev"
-            }
-        }
-    }
-}
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "primaryclient",
+        mongodb_password: "ppclient",
+        mongodb_cluster: "cluster0",
+        mongodb_database: "my-site-dev",
+      },
+    };
+  }
+  return {
+    env: {
+      nothing: "nothing",
+    },
+  };
+};
